Softmax backward fixed + tests added

src/layers/Softmax/Softmax.py
@@ -15,9 +15,14 @@ class Softmax(Layer):
         return self.out
 
     def backward(self, dy):
-        dX = np.zeros_like(self.X)
-        for i in range(dy.shape[1]):  # Iterate over the batch
-            y = self.out[:, i].reshape(-1, 1)
-            jacobian = np.diagflat(y) - np.dot(y, y.T)
-            dX[:, i] = np.dot(jacobian, dy[:, i])
-        return dX
+        # Reshape self.out and dy for broadcasting
+        softmax_out = self.out[:, :, np.newaxis]
+        dy = dy[:, np.newaxis, :]
+
+        # Compute the Jacobian matrix efficiently
+        jacobian = softmax_out * (np.eye(self.out.shape[1])[np.newaxis, :, :] - softmax_out.transpose(0, 2, 1))
+
+        # Compute the gradient
+        gradient = np.einsum('ijk,ik->ij', jacobian, dy.squeeze(axis=1))
+
+        return gradient

tests/unit/test_softmax.py
@@ -24,5 +24,21 @@ class MyTestCase(unittest.TestCase):
                          [self.e ** (-2), self.e ** (-1), 1] / np.sum([self.e ** (-2), self.e ** (-1), 1])])
         assert_array_equal(res2, output2, "Wrong forward output for regular case")
 
-        if __name__ == '__main__':
-            unittest.main()
+    def testBackward(self):
+        x1 = np.array([[1, 1]])
+        y1 = np.array([[1, 0]])
+        y2 = np.array([[0, 1]])
+        dx1 = np.array([[0.25, -0.25]])
+        dx2 = np.array([[-0.25, 0.25]])
+        self.softmax.forward(x1)
+        assert_array_equal(dx1, self.softmax.backward(y1), "Wrong backward")
+        assert_array_equal(dx2, self.softmax.backward(y2), "Wrong backward")
+
+        x2 = np.array([[1, 1], [1, 1]])
+        self.softmax.forward(x2)
+        assert_array_equal(np.concatenate((dx1, dx2), axis=0), self.softmax.backward(np.concatenate((y1, y2), axis=0)),
+                           "Wrong backward with multiple vectors")
+
+
+if __name__ == '__main__':
+    unittest.main()