a35c25166dd2a7f471e8c30cd24190564ca5c0f0
ivansmirnov <i.smirnov@staff.easychair.org>
2024-07-13 15:40:42 +0100

    Typo

--- tests/unit/test_batch_index_generator.py
+++ tests/unit/test_batch_index_generator.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from src.train.batch_index_generator import ButchIndexGenerator
+from src.train.batch_index_generator import BatchIndexGenerator
 
 
 class MyTestCase(unittest.TestCase):
@@ -11,14 +11,14 @@ class MyTestCase(unittest.TestCase):
         self.batch_size = 3
 
     def test_iterator_size(self):
-        ig = ButchIndexGenerator(self.n_size, self.batch_size)
+        ig = BatchIndexGenerator(self.n_size, self.batch_size)
         self.assertEqual(3, len(ig.next()))
         self.assertEqual(3, len(ig.next()))
         self.assertEqual(3, len(ig.next()))
         self.assertEqual(1, len(ig.next()))
 
     def test_iterator_unique_values(self):
-        ig = ButchIndexGenerator(self.n_size, self.batch_size)
+        ig = BatchIndexGenerator(self.n_size, self.batch_size)
         values = [idx for _ in range(4) for idx in ig.next()]
         self.assertEqual(10, len(np.unique(values)))
 
